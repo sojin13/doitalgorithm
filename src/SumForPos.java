d0074b2f417345a2d1b6f7a847f2bcac09100369
sojin13 <sodyniee@naver.com>
2019-03-17 22:09:51 +0900

    chpater01 codes commit

---
+++ src/SumForPos.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class SumForPos {
+    public static void main(String [] args) {
+        Scanner stdIn =  new Scanner(System.in);
+
+        System.out.println("1부터 n까지의 합을 구합니다.");
+        System.out.println("n의 값 : ");
+        int n;
+        int sum;
+
+        do {
+            System.out.println("n의 값 : ");
+            n = stdIn.nextInt();
+        } while ( n <= 0 ); {
+            sum = 0;
+
+            for (int i =1; i <= n; i++) {
+                sum += i;
+            }
+        }
+
+        System.out.println("1부터" + n + "까지의 합은 " + sum + "입니다.");
+    }
+}
